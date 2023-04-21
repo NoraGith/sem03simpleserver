9151d21d7c032953b03928a0f626a514cebf33bb
NoraGith <noraolo@uia.no>
2023-04-21 09:02:57 +0000

    endring i filene og gjort sniffing paa nytt

--- tcp_server.go
+++ tcp_server.go
@@ -36,7 +36,10 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n]))), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switch msg := string(dekrypterMelding); msg { 
+
   				        case "ping":
 						_, err = c.Write([]byte("pong"))
 					default:
